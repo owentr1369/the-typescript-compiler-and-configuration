bfaa6a23fa7c0d4d074a3bc315689356e3f9881b
TranTam <tamvaa1306@gmail.com>
2022-09-15 21:50:15 +0700

    module

--- dist/app.js
+++ dist/app.js
@@ -1,5 +1,15 @@
 "use strict";
 const button = document.querySelector("button");
-button.addEventListener("click", () => {
-    console.log("Click");
-});
+function clickHandler(message, age) {
+    // let userName = "Max"
+    console.log("Clicked " + message + " " + age);
+}
+function add(n1, n2) {
+    if (n1 + n2 > 0) {
+        return n1 + n2;
+    }
+    return;
+}
+if (button) {
+    button.addEventListener("click", clickHandler.bind(null, "You're welcome", 30));
+}
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,20 @@
 const button = document.querySelector("button") as HTMLButtonElement;
 
-button.addEventListener("click", () => {
-  console.log("Click");
-});
+function clickHandler(message: string, age: number) {
+  // let userName = "Max"
+  console.log("Clicked " + message + " " + age);
+}
+
+function add(n1: number, n2: number) {
+  if (n1 + n2 > 0) {
+    return n1 + n2;
+  }
+  return;
+}
+
+if (button) {
+  button.addEventListener(
+    "click",
+    clickHandler.bind(null, "You're welcome", 30)
+  );
+}
